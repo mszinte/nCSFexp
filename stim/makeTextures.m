ff86db29da1c78f6adecfd8c246fdf14866452dc
Uriel LASCOMBES uriel.lascombes@univ-amu.fr <ulascombes@login02.cluster>
2025-01-30 17:00:24 +0100

    work on makeTexture

--- stim/makeTextures.m
+++ stim/makeTextures.m
@@ -86,15 +86,15 @@ rect_noise              =   const.rect_noise;
 
 % compute total amount of picture to print
 total_amount            =   ((kappa_val_num-1) * noise_rand_num * stim_ori_num * sp_cut_num * mc_cut_num) + ...
+    (noise_rand_num * stim_ori_num * sp_cut_num * mc_cut_num)
                             1;
 
 textprogressbar('Progress: ');
 
 for sp_val_stim = 1:sp_cut_num
     for contrast_val_stim = 1:mc_cut_num
-        for kappa_val_stim = 15:kappa_val_num
+        for kappa_val_stim = 2:kappa_val_num
             for noise_rand = 1:noise_rand_num
-                
                 % make stim texture full screen
                 sp_sigma_val            =   const.sp_cutSigma;
                 sp_center_val           =   const.sp_cutCenters(sp_val_stim);
@@ -107,36 +107,38 @@ for sp_val_stim = 1:sp_cut_num
                 noise_patch(:,:,2)      =   mat_noise*const.stim_color(2);
                 noise_patch(:,:,3)      =   mat_noise*const.stim_color(3);
                 noise_patch(:,:,4)      =   const.stim_aperture;
-                
+
                 tex_stim                =   Screen('MakeTexture',scr.main,noise_patch);
                 clear noise_patch
-                
+
                 for stim_ori = 1:stim_ori_num
                     % define all texture parameters
                     rects                   =   [   rect_noise,...                                                  % stim noise
                                                     rect_noise,...                                                  % fix annulus
                                                     rect_noise,...                                                  % empty center
                                                     rect_noise];                                                    % fixation dot
-                    
+
                     texs                    =   [   tex_stim,...                                                    % stim noise
                                                     tex_fix_ann_probe,...                                           % fix anulus
                                                     tex_black_fix_noise,...                                         % empty center
                                                     tex_fix_dot_probe];                                             % fixation dot
-                    
+
                     angles                  =   [   const.noise_angle(stim_ori),...                                 % stim noise
                                                     0,...                                                           % fix anulus
                                                     0,...                                                           % empty center
                                                     0];                                                             % fixation dot
-                    
+
                     % draw all textures
                     Screen('FillRect',scr.main,const.background_color);
                     Screen('DrawTextures',scr.main,texs,[],rects,angles)
+                    % Screen('FillRect',scr.main,const.background_color,const.left_propixx_hide)
+                    % Screen('FillRect',scr.main,const.background_color,const.right_propixx_hide)
                     Screen('DrawLines',scr.main, const.line_fix_up_left, const.line_width, const.white, [], 1);
                     Screen('DrawLines',scr.main, const.line_fix_up_right, const.line_width, const.white, [], 1);
                     Screen('DrawLines',scr.main, const.line_fix_down_left, const.line_width, const.white, [], 1);
                     Screen('DrawLines',scr.main, const.line_fix_down_right, const.line_width, const.white, [], 1);
                     Screen('DrawingFinished',scr.main,[],1);
-                    
+
                     if const.drawStimuli
                         % plot and save the sreenshot
                         Screen('Flip',scr.main);
@@ -146,142 +148,128 @@ for sp_val_stim = 1:sp_cut_num
                         screen_stim             =   Screen('GetImage', scr.main,const.stim_rect,'backBuffer',[],1);
                     end
                     screen_filename         =   sprintf('%s/%sprobe_spStim%i_contStim%i_kappaStim%i_stimOri-%i_noiseRand%i.mat',...
-                        const.stim_folder,sp_val_stim,contrast_val_stim,kappa_val_stim,stim_ori,noise_rand);
+                        const.stim_folder, sp_val_stim, contrast_val_stim, kappa_val_stim, stim_ori, noise_rand);
                     save(screen_filename,'screen_stim')
                     clear screen_stim
-                    
+
                     numPrint                =   numPrint+1;
                     textprogressbar(numPrint*100/total_amount);
 
             end
             % close opened texture
             Screen('Close',tex_stim);
-            
+
             end
         end
     end
 end
 
-% for noise_rand = 1:noise_rand_num
-% 
-%     % make stim texture
-%     mat_noise               =   genNoisePatch(const,const.noise_kappa(1));
-%     mat_noise        =   imresize(mat_noise,round([const.noise_size,const.noise_size]),'bilinear');
-%     noise_patch(:,:,1)      =   mat_noise*const.stim_color(1);
-%     noise_patch(:,:,2)      =   mat_noise*const.stim_color(2);
-%     noise_patch(:,:,3)      =   mat_noise*const.stim_color(3);
-%     noise_patch(:,:,4)      =   const.stim_aperture;
-%     bar_dir_list            =   [1,3];
-% 
-%     tex_stim                =   Screen('MakeTexture',scr.main,noise_patch);
-%     clear noise_patch
-% 
-%     for bar_dir = bar_dir_list
-%         if bar_dir == 1
-%             bar_step_num = bar_step_num_hor;
-%             bar_aperture_rect = const.bar_aperture_rect_hor;
-%             dir_txt = 'hor_';
-%         elseif bar_dir == 3
-%             bar_step_num = bar_step_num_ver;
-%             bar_aperture_rect = const.bar_aperture_rect_ver;
-%             dir_txt = 'ver_';
-%         end
-% 
-%         for bar_step = 1:bar_step_num
-% 
-%             % define all texture parameters
-%             rects                   =   [   rect_noise,...                                                  % stim noise
-%                                             bar_aperture_rect(:,:,bar_step,bar_dir)',...                    % bar aperture
-%                                             rect_noise,...                                                  % fix annulus
-%                                             rect_noise,...                                                  % empty center
-%                                             rect_noise];                                                    % fixation dot
-% 
-%             texs                    =   [   tex_stim,...                                                    % stim noise
-%                                             tex_bar_aperture,...                                            % bar aperture
-%                                             tex_fix_ann_no_probe,...                                        % fix anulus
-%                                             tex_black_fix_noise,...                                         % empty center
-%                                             tex_fix_dot_no_probe];                                          % fixation dot
-% 
-%             angles                  =   [   0,...                                                           % stim noise
-%                                             -const.bar_dir_ang(bar_dir)+90,...                              % bar aperture
-%                                             0,...                                                           % fix anulus
-%                                             0,...                                                           % empty center
-%                                             0];                                                             % fixation dot
-% 
-% 
-%             % draw all textures
-%             Screen('FillRect',scr.main,const.background_color);
-%             Screen('DrawTextures',scr.main,texs,[],rects,angles)
-%             Screen('FillRect',scr.main,const.background_color,const.left_propixx_hide)
-%             Screen('FillRect',scr.main,const.background_color,const.right_propixx_hide)
-%             Screen('DrawLines',scr.main, const.line_fix_up_left, const.line_width, const.white, [], 1);
-%             Screen('DrawLines',scr.main, const.line_fix_up_right, const.line_width, const.white, [], 1);
-%             Screen('DrawLines',scr.main, const.line_fix_down_left, const.line_width, const.white, [], 1);
-%             Screen('DrawLines',scr.main, const.line_fix_down_right, const.line_width, const.white, [], 1);
-%             Screen('DrawingFinished',scr.main,[],1);
-% 
-%             if const.drawStimuli
-%                 % plot save the sreenshot
-%                 Screen('Flip',scr.main);
-%                 screen_stim             =   Screen('GetImage', scr.main,const.stim_rect,[],0,1);
-%             else
-%                 % save the sreenshot
-%                 screen_stim             =   Screen('GetImage', scr.main,const.stim_rect,'backBuffer',[],1);
-%             end
-%             screen_filename         =   sprintf('%s/%snoprobe_barStep-%i_noiseRand%i.mat',...
-%                 const.stim_folder,dir_txt,bar_step,noise_rand);
-% 
-%             %save(screen_filename,'screen_stim','-v7.3','-nocompression')
-%             save(screen_filename,'screen_stim')
-%             clear screen_stim
-% 
-%             numPrint                =   numPrint+1;
-%             textprogressbar(numPrint*100/total_amount);
-% 
-%         end
-%     end
-%     % close opened texture
-% 	Screen('Close',tex_stim);
-% end
-% 
-% % make inter-interval screenshot
-% rects                   =   [rect_noise,...                                                                         % fix annulus
-%                              rect_noise,...                                                                         % empty center
-%                              rect_noise];                                                                           % fixation dot
-% texs                    =   [tex_fix_ann_no_probe,...                                                               % fix annulus
-%                              tex_black_fix_noise,...                                                                % empty center
-%                              tex_fix_dot_no_probe];                                                                 % fixation dot
-% 
-% Screen('FillRect',scr.main,const.background_color);
-% Screen('DrawTextures',scr.main,texs,[],rects)
+for sp_val_stim = 1:sp_cut_num
+    for contrast_val_stim = 1:mc_cut_num
+        for noise_rand = 1:noise_rand_num
+            % make stim texture
+            sp_sigma_val            =   const.sp_cutSigma;
+            sp_center_val           =   const.sp_cutCenters(sp_val_stim);
+            contrast_val            =   const.mc_intervals(contrast_val_stim);
+            kappa_val               =   const.noise_kappa(1);
+
+            mat_noise               =   genNoisePatch(const, sp_center_val, sp_sigma_val, kappa_val, contrast_val);
+
+            noise_patch(:,:,1)      =   mat_noise*const.stim_color(1);
+            noise_patch(:,:,2)      =   mat_noise*const.stim_color(2);
+            noise_patch(:,:,3)      =   mat_noise*const.stim_color(3);
+            noise_patch(:,:,4)      =   const.stim_aperture;
+
+            tex_stim                =   Screen('MakeTexture',scr.main,noise_patch);
+            clear noise_patch
+
+            % define all texture parameters
+            rects                   =   [   rect_noise,...                                                  % stim noise
+                                            rect_noise,...                                                  % fix annulus
+                                            rect_noise,...                                                  % empty center
+                                            rect_noise];                                                    % fixation dot
+
+            texs                    =   [   tex_stim,...                                                    % stim noise
+                                            tex_fix_ann_no_probe,...                                        % fix anulus
+                                            tex_black_fix_noise,...                                         % empty center
+                                            tex_fix_dot_no_probe];                                          % fixation dot
+
+            angles                  =   [   0,...                                                           % stim noise
+                                            0,...                                                           % fix anulus
+                                            0,...                                                           % empty center
+                                            0];                                                             % fixation dot
+
+
+            % draw all textures
+            Screen('FillRect',scr.main,const.background_color);
+            Screen('DrawTextures',scr.main,texs,[],rects,angles)
+            % Screen('FillRect',scr.main,const.background_color,const.left_propixx_hide)
+            % Screen('FillRect',scr.main,const.background_color,const.right_propixx_hide)
+            Screen('DrawLines',scr.main, const.line_fix_up_left, const.line_width, const.white, [], 1);
+            Screen('DrawLines',scr.main, const.line_fix_up_right, const.line_width, const.white, [], 1);
+            Screen('DrawLines',scr.main, const.line_fix_down_left, const.line_width, const.white, [], 1);
+            Screen('DrawLines',scr.main, const.line_fix_down_right, const.line_width, const.white, [], 1);
+            Screen('DrawingFinished',scr.main,[],1);
+
+            if const.drawStimuli
+                % plot save the sreenshot
+                Screen('Flip',scr.main);
+                screen_stim             =   Screen('GetImage', scr.main,const.stim_rect,[],0,1);
+            else
+                % save the sreenshot
+                screen_stim             =   Screen('GetImage', scr.main,const.stim_rect,'backBuffer',[],1);
+            end
+            screen_filename         =   sprintf('%s/%snoprobe_spStim%i_contStim%i_noiseRand%i.mat',...
+                        const.stim_folder, sp_val_stim, contrast_val_stim, noise_rand);
+
+            save(screen_filename,'screen_stim')
+            clear screen_stim
+
+            numPrint                =   numPrint+1;
+            textprogressbar(numPrint*100/total_amount);
+
+            % close opened texture
+	        Screen('Close',tex_stim);
+        end
+    end
+end
+
+% make inter-interval screenshot
+rects                   =   [rect_noise,...                                                                         % fix annulus
+                             rect_noise,...                                                                         % empty center
+                             rect_noise];                                                                           % fixation dot
+texs                    =   [tex_fix_ann_no_probe,...                                                               % fix annulus
+                             tex_black_fix_noise,...                                                                % empty center
+                             tex_fix_dot_no_probe];                                                                 % fixation dot
+
+Screen('FillRect',scr.main,const.background_color);
+Screen('DrawTextures',scr.main,texs,[],rects)
 % Screen('FillRect',scr.main,const.background_color,const.left_propixx_hide)
 % Screen('FillRect',scr.main,const.background_color,const.right_propixx_hide)
-% Screen('DrawLines',scr.main, const.line_fix_up_left, const.line_width, const.white, [], 1);
-% Screen('DrawLines',scr.main, const.line_fix_up_right, const.line_width, const.white, [], 1);
-% Screen('DrawLines',scr.main, const.line_fix_down_left, const.line_width, const.white, [], 1);
-% Screen('DrawLines',scr.main, const.line_fix_down_right, const.line_width, const.white, [], 1);
-% Screen('DrawingFinished',scr.main,[],1);
-% 
-% if const.drawStimuli
-%     % plot and save the screenshot
-% 	Screen('Flip',scr.main);
-% 	screen_stim             =   Screen('GetImage', scr.main,const.stim_rect,[],0,1);
-% else
-%     % save the sreenshot
-%     screen_stim             =   Screen('GetImage', scr.main,const.stim_rect,'backBuffer',[],1);
-% end
-% screen_filename         =   sprintf('%s/blank.mat',const.stim_folder);
-% %save(screen_filename,'screen_stim','-v7.3','-nocompression')
-% save(screen_filename,'screen_stim')
-% clear screen_stim
-% 
-% numPrint                =   numPrint+1;
-% textprogressbar(numPrint*100/total_amount);
-% textprogressbar(numPrint*100/total_amount);
+Screen('DrawLines',scr.main, const.line_fix_up_left, const.line_width, const.white, [], 1);
+Screen('DrawLines',scr.main, const.line_fix_up_right, const.line_width, const.white, [], 1);
+Screen('DrawLines',scr.main, const.line_fix_down_left, const.line_width, const.white, [], 1);
+Screen('DrawLines',scr.main, const.line_fix_down_right, const.line_width, const.white, [], 1);
+Screen('DrawingFinished',scr.main,[],1);
+
+if const.drawStimuli
+    % plot and save the screenshot
+	Screen('Flip',scr.main);
+	screen_stim             =   Screen('GetImage', scr.main,const.stim_rect,[],0,1);
+else
+    % save the sreenshot
+    screen_stim             =   Screen('GetImage', scr.main,const.stim_rect,'backBuffer',[],1);
+end
+screen_filename         =   sprintf('%s/blank.mat',const.stim_folder);
+save(screen_filename,'screen_stim')
+clear screen_stim
+
+numPrint                =   numPrint+1;
+textprogressbar(numPrint*100/total_amount);
+textprogressbar(numPrint*100/total_amount);
 
 % Close all textures
 Screen('Close',tex_black_fix_noise);
-Screen('Close',tex_bar_aperture);
 Screen('Close',tex_fix_ann_probe);
 Screen('Close',tex_fix_ann_no_probe);
 Screen('Close',tex_fix_dot_no_probe);
