747c95cb664f39b193c38f78fafe5d38008fbefa
Uriel LASCOMBES uriel.lascombes@univ-amu.fr <ulascombes@login02.cluster>
2025-03-13 12:26:41 +0100

    psychophy experiment

--- experiment_code/config/keyConfig.m
+++ experiment_code/config/keyConfig.m
@@ -18,13 +18,13 @@ function [my_key]=keyConfig(const)
 
 KbName('UnifyKeyNames');
 
-if const.training
+if const.scanner == 0
     my_key.mri_trVal = 's';                                                 % mri trigger letter
     my_key.left1Val = 'r';                                                  % left button 1
     my_key.left2Val = 'q';                                                  % left button 2
     my_key.right1Val = 'u';                                                 % right button 1
     my_key.right2Val = 'm';                                                 % right button 2
-else
+if const.scanner == 1
     my_key.mri_trVal = 't';                                                 % mri trigger letter
     my_key.left1Val = 'r';                                                  % left button index (inside)
     my_key.left2Val = 'g';                                                  % left button thumb (outside)
